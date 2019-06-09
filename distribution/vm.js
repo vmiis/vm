1be12528916f65a5cc9c1433a283833769688ac0
vmiis <zzm.vmiis@gmail.com>
2019-06-10 00:03:52 +1000

    update

--- distribution/vm.js
+++ distribution/vm.js
@@ -255,14 +255,15 @@ $vm.render_checkbox_field=function(record,mID,$div,html){
 $vm.init=function(callback){
     $vm.user_name="guest";
     $vm.user_id="-";
-    $vm.user_ip="0";
+	$vm.user_ip="0";
+	$vm.sys_N=0;
+	if(callback==undefined) return;
 	
 	if($vm.api_type=="sqlserver"){
 		$vm.init_s(callback);
 		return;		
 	}
 	
-	$vm.sys_N=0;
     $vm.request({cmd:'user-info'},function(res){
         $vm.user_name=res.records[0].user_name;
         if(callback!==undefined) callback(res);
