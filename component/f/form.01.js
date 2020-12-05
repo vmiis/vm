8cfa6238317de4aa6fa68a622c0ec5107d0820ed
Zhiming Zhou <zzm.vmiis@gmail.com>
2020-12-06 08:38:37 +1100

    update

--- component/f/form.01.js
+++ component/f/form.01.js
@@ -522,3 +522,9 @@ $('#pdf__ID').on('click',function(){
     
 })
 //-------------------------------------
+$('#nav__ID a').on('click',function(e){
+    e.preventDefault();
+    var module_name=$(this).attr('href');
+    if(module_name!="") $vm.show_module(m.prefix+module_name,{})
+})
+//--------------------------------------------------------
