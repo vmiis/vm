a85701697d5db0f60410ca60395a45a1ed8454cf
Zhiming Zhou <zzm.vmiis@gmail.com>
2021-01-18 20:25:53 +1100

    update

--- javascript/serialize.js
+++ javascript/serialize.js
@@ -1,4 +1,62 @@
 //--------------------------------------------------------
+$vm.add_value_to_select=function($el, v){
+	var exists = false;
+	$el.find('option').each(function(index){
+		if (this.value == v) {
+			exists = true;
+			return false;
+		}
+	});    
+	if(exists==false){
+		$el.append(new Option(v, v));
+	}
+}
+$vm.deserialize=function(record,form_id){
+	if(record==undefined || record.Data==undefined) return;
+	$.each(record.Data, function(name, value){
+		if(name!=''){
+			var $els = $(form_id+' *[name='+name+']');
+			$els.each(function(){
+				var $el=$(this);
+				var type = $el.attr('type');
+				switch(type){
+					case 'checkbox':
+						if(value=='off' || value=='0' || value=='' || value==undefined ) $el.prop('checked', false);
+						else $el.prop('checked', true);
+						break;
+					case 'radio':
+						if($el.attr('value')==value){
+							$el.prop('checked', true);
+						}
+						break;
+					case 'file':
+						break;
+					case 'text':
+					case 'email':
+					case 'textarea':
+					case 'select':
+						$el.val(value);
+						break;
+					case 'undefined':
+						break;
+					default:
+						var type1=$(this).prop('nodeName');
+						switch(type1){
+							case "SELECT":
+								$vm.add_value_to_select($el,value);
+								$el.val(value);
+								break;
+							default: 
+								console.log(type+"---"+type1)				
+								$el.val(value); break;
+						}
+						break;
+				}
+			});
+		}
+	});
+}
+/*
 $vm.deserialize=function(record,form_id){
     if(record==undefined || record.Data==undefined) return;
     $.each(record.Data, function(name, value){
@@ -36,6 +94,7 @@ $vm.deserialize=function(record,form_id){
         }
     });
 }
+*/
 $vm.deserialize_s=function(record,form_id){
     if(record==undefined) return;
     $.each(record, function(name, value){
